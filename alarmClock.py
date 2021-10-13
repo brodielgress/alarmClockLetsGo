18bba7b101f8e4f0ead4e202ef57034a239083b2
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-10-13 19:57:30 -0400

    Added some placeholder functions

--- alarmClock.py
+++ alarmClock.py
@@ -3,6 +3,10 @@ import datetime
 now = datetime.datetime.now()
 display_time = now.strftime("%H:%M:%S")
 
+def user_inputs_alarm():
+    #todo create a function to allow a user to input a time for alarm
+    pass
+
 def convert_user_time():
     #todo Figure out how to convert user's input into datetime format: https://www.educative.io/edpresso/how-to-convert-a-string-to-a-date-in-python
     pass
