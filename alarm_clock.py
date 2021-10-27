6dbf66f3574cf14cef58bab635acc544b830b140
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-10-27 19:31:37 -0400

    Made marginal changes

--- alarm_clock.py
+++ alarm_clock.py
@@ -3,15 +3,11 @@ from datetime import datetime
 now = datetime.now()
 display_time = now.strftime('%H:%M')
 
-    #todo create a function to allow a user to input a time for alarm
-def user_inputs_alarm():
-    pass
-
     #todo Figure out how to convert user's input into datetime format: https://www.educative.io/edpresso/how-to-convert-a-string-to-a-date-in-python
 def convert_user_time():
-    user_input_str = str(input('Please enter a time for your alarm(yyyy-mm-dd HH:MM): '))
+    user_input_str = str(input('Please enter a time for your alarm(HH:MM): '))
 
-    date_time_obj = datetime.strptime(user_input_str, '%Y-%m-%d %H:%M')
+    date_time_obj = datetime.strptime(user_input_str, '%H:%M')
 
     print ('Your set alarm time is', date_time_obj)
     return date_time_obj
@@ -20,11 +16,12 @@ def convert_user_time():
 def time_check():
     converted_time = convert_user_time()
     print(converted_time)
+    print(display_time)
+    print('The time now is', display_time)
 
-    if converted_time == now:
+    if converted_time == display_time:
         print('Wake up!')
     else:
-        print('The time now is', display_time)
         print('zzz...')
     
 time_check()
