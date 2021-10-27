6f6b374d7316b3678fb5c6fb78f1bb6f26c2b775
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-10-27 17:34:56 -0400

    Got code to convert user's input ... but not really? It just spells out how they should input the code?

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,23 +1,30 @@
-import datetime
+from datetime import datetime
 
-now = datetime.datetime.now()
-display_time = now.strftime("%H:%M:%S")
+now = datetime.now()
+display_time = now.strftime('%H:%M')
 
-def user_inputs_alarm():
     #todo create a function to allow a user to input a time for alarm
+def user_inputs_alarm():
     pass
 
-def convert_user_time():
     #todo Figure out how to convert user's input into datetime format: https://www.educative.io/edpresso/how-to-convert-a-string-to-a-date-in-python
-    pass
+def convert_user_time():
+    user_input_str = str(input('Please enter a time for your alarm(yyyy-mm-dd HH:MM): '))
 
-def time_check():
+    date_time_obj = datetime.strptime(user_input_str, '%Y-%m-%d %H:%M')
+
+    print ('Your set alarm time is', date_time_obj)
+    return date_time_obj
+
     #todo Check out timezone import.
+def time_check():
     converted_time = convert_user_time()
+    print(converted_time)
+
     if converted_time == now:
-        print("Wake up!")
+        print('Wake up!')
     else:
-        print(display_time)
-        print("zzz...")
+        print('The time now is', display_time)
+        print('zzz...')
     
 time_check()
